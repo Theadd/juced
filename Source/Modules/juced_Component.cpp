a5b06182c38344a5c345199e6486d8d4c3c39317
dang03 <dang03@hotmail.com>
2013-05-17 11:42:13 +0200

    Initial implementation of juced_Component

---
+++ Source/Modules/juced_Component.cpp
@@ -0,0 +1,57 @@
+/*
+  ==============================================================================
+
+    juced_Component.cpp
+    Created: 15 May 2013 4:26:01pm
+    Author:  admin
+
+  ==============================================================================
+*/
+
+#include "juced_Component.h"
+
+int juced_Component::_numObjects = 0; 
+
+juced_Component::juced_Component()
+{
+    //setSize (500, 400);
+	setName("Container");
+	setProperty(Attributes::objectType, Modules::Component.toString());
+	setProperty(Attributes::height, getHeight());
+	setProperty(Attributes::width, getWidth());
+	setProperty(Attributes::name, getName());
+	++_numObjects;
+	setProperty(Attributes::varName, getName() + String(_numObjects));
+	setProperty(Attributes::className, "Component");
+	setProperty(Attributes::toolName, "juced_Component");
+	setProperty(Attributes::declareExtended, false);
+	setComponentID(Uuid().toString());
+	setProperty(Attributes::ID, getComponentID());
+	setProperty(Attributes::allowTransform, false);
+}
+
+juced_Component::~juced_Component()
+{
+}
+
+/*void juced_MainComponent::paint (Graphics& g)
+{
+    g.fillAll (Colours::transparentWhite);
+
+}*/
+
+void juced_Component::resized()
+{
+    // This is called when the MainContentComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
+
+void juced_Component::mouseUp (const MouseEvent& event) {}
+void juced_Component::mouseDrag (const MouseEvent& event) {}
+void juced_Component::mouseDoubleClick (const MouseEvent& event) {}
+void juced_Component::mouseMove (const MouseEvent& event) {}
+void juced_Component::mouseDown (const MouseEvent& event) {}
+void juced_Component::mouseEnter (const MouseEvent& event) {}
+void juced_Component::mouseExit (const MouseEvent& event) {}
+void juced_Component::mouseWheelMove (const MouseEvent &event, const MouseWheelDetails &wheel) {}
---
+++ Source/Modules/juced_Component.h
@@ -0,0 +1,48 @@
+/*
+  ==============================================================================
+
+    juced_Component.h
+    Created: 15 May 2013 4:26:01pm
+    Author:  admin
+
+  ==============================================================================
+*/
+
+#ifndef __JUCED_COMPONENT_H_6B6D7789__
+#define __JUCED_COMPONENT_H_6B6D7789__
+
+#include "../Designer/Globals.h"
+
+class juced_Component : public Component,
+							public DynamicObject
+{
+public:
+    
+	//ValueTree *linkedValueTree;
+
+    juced_Component();
+    ~juced_Component();
+
+    //void paint(Graphics&);
+    void resized();
+
+	void mouseUp (const MouseEvent& event);
+	void mouseDrag (const MouseEvent& event);
+	void mouseDoubleClick (const MouseEvent& event);
+	void mouseMove (const MouseEvent& event);
+	void mouseDown (const MouseEvent& event);
+	void mouseEnter (const MouseEvent& event);
+	void mouseExit (const MouseEvent& event);
+	void mouseWheelMove (const MouseEvent &event, const MouseWheelDetails &wheel);
+
+private:
+	static int _numObjects;
+};
+
+
+#endif  // __JUCED_MAINCOMPONENT_H_1B08AA79__
+
+
+
+
+#endif  // __JUCED_COMPONENT_H_6B6D7789__
